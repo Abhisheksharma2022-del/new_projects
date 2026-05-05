7d2e2301ab6e54bd40b3e983e34ef4432b54da76
abhi.py <abhibhardwaj7271@gmail.com>
2026-05-05 14:25:42 +0530

    Update balance after withdrawal

---
+++ 05_q5.py
@@ -0,0 +1,11 @@
+# Initial balance
+balance = 12000
+
+# Withdrawal amount
+withdrawal = 3000
+
+# Update balance
+balance = balance - withdrawal
+
+# Output
+print("After withdrawal, balance =", balance)
