d21185ffc7c57e6a68d3bc8a0bbde861fff8a4a4
abhi.py <abhibhardwaj7271@gmail.com>
2026-05-05 15:08:23 +0530

    Calculate student percentage

---
+++ 08_q8.py
@@ -0,0 +1,7 @@
+# percentage calculator 
+print("percentage calculator ")
+x=float(input("Obtain marks::: ")) 
+y=float(input("Total marks::: ")) 
+
+z = (x/y)*100
+print(f"Total percentage:::{z}%")
