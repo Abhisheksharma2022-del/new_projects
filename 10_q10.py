539c58d01bc8c0f6482ddf33ffbd049e17dc84fe
abhi.py <abhibhardwaj7271@gmail.com>
2026-05-12 20:20:16 +0530

    10_q10.py

---
+++ 10_q10.py
@@ -0,0 +1,17 @@
+# Switch values of two strings
+
+char1 = "hello"
+char2 = "java"
+
+print("Before Swapping:")
+print("char1 =", char1)
+print("char2 =", char2)
+
+# swapping values
+temp = char1
+char1 = char2
+char2 = temp
+
+print("\nAfter Swapping:")
+print("char1 =", char1)
+print("char2 =", char2)
