25ec697936f76889da5d7103ad43b34514527ffc
abhi.py <abhibhardwaj7271@gmail.com>
2026-05-12 20:32:19 +0530

    Calculate Average of 3 Numbers

---
+++ 14_q14.py
@@ -0,0 +1,16 @@
+# Calculate Average of 3 Numbers
+
+num1 = 25
+num2 = 35
+num3 = 45
+
+# calculate total
+total = num1 + num2 + num3
+
+# calculate average
+average = total / 3
+
+print("Number 1 =", num1)
+print("Number 2 =", num2)
+print("Number 3 =", num3)
+print("Average =", average)
