15700e8816be9d23c7e199ec6ecbddc0dff15ce3
abhi.py <abhibhardwaj7271@gmail.com>
2026-05-12 20:30:11 +0530

    Calculate Student Percentage

---
+++ 13_q13.py
@@ -0,0 +1,11 @@
+# Calculate Student Percentage
+
+obtain_marks = 430
+out_of = 500
+
+# percentage formula
+percentage = (obtain_marks / out_of) * 100
+
+print("Obtain Marks =", obtain_marks)
+print("Total Marks =", out_of)
+print("Percentage =", percentage, "%")
