b8fbd035338f0c75a4f1be48b1fe7699a4826693
abhi.py <abhibhardwaj7271@gmail.com>
2026-05-12 21:12:40 +0530

    Define input variables

---
+++ 16_q16.py
@@ -0,0 +1,10 @@
+# Define input variables
+principal = 10000
+rate = 5
+time = 2
+
+# Calculate simple interest
+simple_interest = (principal * rate * time) / 100
+
+# Display the output
+print("Simple Interest =", simple_interest)
