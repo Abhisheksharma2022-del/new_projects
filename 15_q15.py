1e2734edfae90e1840b7f4ece60429f703f1f31b
abhi.py <abhibhardwaj7271@gmail.com>
2026-05-12 21:05:18 +0530

    calculate profit and loss percentag

--- 15_q15.py
+++ 15_q15.py
@@ -1,22 +1,32 @@
-# calculate profit
-profit = selling_price - cost_price
-    
-    # calculate profit percentage
-profit_percentage = (profit / cost_price) * 100
-    
-print("Profit =", profit)
-print("Profit Percentage =", profit_percentage, "%")
+# calculate profit and loss percentage
 
+cost_price = 500
+selling_price = 600
+
+# check profit
+if selling_price > cost_price:
+
+    # calculate profit
+    profit = selling_price - cost_price
+
+    # calculate profit percentage
+    profit_percentage = (profit / cost_price) * 100
+
+    print("Profit =", profit)
+    print("Profit Percentage =", profit_percentage, "%")
+
+# check loss
 elif cost_price > selling_price:
-    
+
     # calculate loss
     loss = cost_price - selling_price
-    
+
     # calculate loss percentage
     loss_percentage = (loss / cost_price) * 100
-    
+
     print("Loss =", loss)
     print("Loss Percentage =", loss_percentage, "%")
 
+# no profit no loss
 else:
     print("No Profit No Loss")
