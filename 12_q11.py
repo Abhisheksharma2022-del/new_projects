e74112cc4ce3bb77defb44b055a459d479a895bd
abhi.py <abhibhardwaj7271@gmail.com>
2026-05-12 20:25:15 +0530

    12_q12.py

---
+++ 12_q11.py
@@ -0,0 +1,13 @@
+# Apply a 20% discount to a price
+
+price = 1000
+
+print("Before:", price)
+
+# calculating 20% discount
+discount = price * 20 / 100
+
+# final price after discount
+price = price - discount
+
+print("After:", price)
