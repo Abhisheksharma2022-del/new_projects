d1db665fe201b72e7c12f55f344e604e32c1138d
abhi.py <abhibhardwaj7271@gmail.com>
2026-05-13 20:59:32 +0530

    Movie Ticket Program

---
+++ 34_q34.py
@@ -0,0 +1,15 @@
+# Movie Ticket Program
+
+movie_name = input("Enter Movie Name: ")
+viewer_name = input("Enter Viewer Name: ")
+seat_number = input("Enter Seat Number: ")
+show_time = input("Enter Show Time: ")
+
+print("\n------------------------")
+print("🎬 Movie Ticket 🎬")
+print("------------------------")
+print("Movie   :", movie_name)
+print("Name    :", viewer_name)
+print("Seat No :", seat_number)
+print("Time    :", show_time)
+print("Enjoy the show!")
