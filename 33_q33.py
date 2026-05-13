8f334a6919cbb8257b4261262e27d1a7c77f9289
abhi.py <abhibhardwaj7271@gmail.com>
2026-05-13 20:57:24 +0530

    Input from user

---
+++ 33_q33.py
@@ -0,0 +1,11 @@
+# Input from user
+name = input("Enter Full Name: ")
+profession = input("Enter Profession: ")
+quote = input("Enter Favorite Quote: ")
+experience = input("Enter Years of Experience: ")
+
+# Output format
+print("\n------------------------------------------------")
+print("Name       :", name)
+print("Profession :", profession)
+print("Experience :", experience, "years")
