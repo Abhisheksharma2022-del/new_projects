47c9322b8f9beec298462ea38046fb115cabbcda
abhi.py <abhibhardwaj7271@gmail.com>
2026-05-13 20:54:33 +0530

    user input is

---
+++ 32_q32.py
@@ -0,0 +1,14 @@
+# User details
+
+name = "Dev"
+age = 25
+city = "Jaipur"
+hobby = "Cooking"
+
+# formatted output
+print("User Profile")
+print("-------------")
+print("Name  :", name)
+print("Age   :", age)
+print("City  :", city)
+print("Hobby :", hobby)
